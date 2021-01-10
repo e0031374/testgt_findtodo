31d6e29c399942c70471705fda68bc16e92d9433
owo <e0031374@gmail.com>
2021-01-10 12:00:50 +0800

    Move main.go to project root directory

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 func main() {
 	pathFlag := flag.String("p", ".", "path root to begin checking files for string")
 	stringFlag := flag.String("s", `"TODO"`, "target string to check files for")
+	exactFlag := flag.Bool("e", true, "should program search for exact match (true) or substring (false)")
+
 	flag.Parse()
 	//ok, err := fileContains.Driver("a.in", "todo")
+	fmt.Println(*exactFlag)
 
 	ok, err := fileContains.Driver(*pathFlag, *stringFlag)
 	if err != nil {
